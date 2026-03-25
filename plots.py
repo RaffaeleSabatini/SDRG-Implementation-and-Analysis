90f877f0461d8aac911dd6d9fdaac45e67bc3653
Raffaele Sabatini <f4ff0@icloud.com>
2026-03-25 18:54:26 +0100

    Refactoring (2)

---
+++ plots.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#------------------------------------------------------------------------
+
+def plot_site_decimation(site_decim_frac, N):
+    '''
+        Plots the fraction of decimated sites (#site decimated/#total decimations) as a function of 
+        remaining sites number.
+    '''
+
+    
