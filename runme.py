61d3ddf3d82c1bfbb4f78b15671d575e2dfb5bc4
Raffaele <f4ff0@icloud.com>
2026-04-04 10:00:02 +0000

    Tests

--- runme.py
+++ runme.py
@@ -4,7 +4,7 @@ from utilities import *
 
 from itertools import product
 
-M     = 5000
+M     = 4
 N     = 2048
 ZETA  = 1
 H0    = np.exp([-3*k for k in range(2, 6)])
@@ -16,4 +16,4 @@ gamma_list = 0.85 + interpol*(1.05-0.85)
 
 for h0, gamma0 in product(H0, gamma_list):
         omega_list, decimations = RandomIsing_SDRG(M, N, gamma0, h0, J_0=1, zeta=ZETA, n_cores=4, DEBUG=False)
-        save_results(omega_list, "crossover-region", gamma0, h0, N, M, 0)
+        save_results(omega_list, "crossover-region2", gamma0, h0, N, M, 0)
