33c180d09f42f2c96f7146fa6bc9f4b337ad5c37
Raffaele Sabatini <f4ff0@icloud.com>
2026-04-17 22:29:20 +0200

    fix

--- runSDRG.py
+++ runSDRG.py
@@ -13,14 +13,11 @@ ZETA   = 1
 H0     = np.exp(-6)
 GAMMA0 = [0.5, 0.8, 0.84, 0.85, 0.86, 0.89, 0.95, 1]
 
-combinations = list(product(GAMMA0, H0, N))
-total_c = len(combinations)
+for i, gamma0 in enumerate(GAMMA0):
+        print(f"\nIteration {i}/{len(GAMMA0)}")
+        omega_list, decimations, magnetic_moment = RandomIsing_SDRG(M, N, gamma0, H0, J_0=1, zeta=ZETA, n_cores=4, DEBUG=False)
 
-for i, (gamma0, h0, n) in enumerate(combinations):
-        print(f"\nIteration {i}/{total_c}")
-        omega_list, decimations, magnetic_moment = RandomIsing_SDRG(M, n, gamma0, h0, J_0=1, zeta=ZETA, n_cores=4, DEBUG=False)
-
-        save_results(magnetic_moment, f"{dir_name}/mag_moments", gamma0, h0, n, M, 0)
-        save_results(omega_list, f"{dir_name}/excitations", gamma0, h0, n, M, 0)
-        save_results(decimations, f"{dir_name}/decimations", gamma0, h0, n, M, 0)
+        save_results(magnetic_moment, f"{dir_name}/mag_moments", gamma0, H0, N, M, 0)
+        save_results(omega_list, f"{dir_name}/excitations", gamma0, H0, N, M, 0)
+        save_results(decimations, f"{dir_name}/decimations", gamma0, H0, N, M, 0)
 
